fbc5c23f062812d87eda87e408031887e70e5a28
CB-Pantomime <christopherbrown.contact@gmail.com>
2023-08-14 14:34:15 -0700

    component to pass into tvf screens.

---
+++ frontend/src/components/tvfPromoteComponent.js
@@ -0,0 +1,29 @@
+
+
+import React from 'react';
+import { Container } from 'react-bootstrap'; 
+
+const linkStyle = {
+    // textDecoration: 'none',
+    // fontFamily: 'EB Garamond',
+    color: 'black',
+    textDecorationLine: 'underline',
+    textDecorationStyle: 'bold',
+    textDecorationThickness: '3px',
+    textUnderlineOffset: '4px',
+    textDecorationColor: 'rgb(214, 109, 167)'
+}
+
+const TVFPromoteComponent = () => {
+    return (
+        <Container className='TVFPromoteComponent-container'>
+           <h1 className='h1-callToAction-televisionFlowersApp'>
+            view the television flowers web application <a href='http://www.televisionflowers.com' target='_blank' style={linkStyle}>
+                  here
+              </a>
+           </h1>
+        </Container>
+    );
+};
+
+export default TVFPromoteComponent;
